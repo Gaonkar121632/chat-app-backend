5e770ba1c5b33914f480b80857b4dc9d5053df79
vinayak_gaonkar <vinayak.gaonkar@mobinius.com>
2020-08-07 00:14:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const { json } = require('express');
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
+  res.send('<h1>Hey Socket.io</h1>');
+});
+
+let allUser={
+
+}
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.id);
+  socket.on('newUser', function (data, sessionId) {
+    allUser[socket.id]=data
+    console.log("new user added",data,socket.id,allUser);
+    io.emit('startPath', JSON.stringify(allUser));
+  });
+
+  socket.on('request', function (data, sessionId) {
+    io.to(data).emit('request', socket.id);
+  });
+
+  socket.on('reject', function (data, sessionId) {
+    io.to(data).emit('reject', socket.id);
+  });
+
+  socket.on('accept', function (data, sessionId) {
+    io.to(data).emit('accept', socket.id);
+  });
+
+  socket.on('send-txt', function (data, sessionId) {
+    console.log("gotit",data);
+    io.to(data.id).emit('send-txt', data.text);
+  });
+
+  socket.on('quit-session', function (data, sessionId) {
+    // console.log("gotit",data);
+    io.to(data).emit('quit-session', "quit this session");
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    delete allUser[socket.id]
+  });
+});
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
